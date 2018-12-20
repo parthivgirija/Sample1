60ef461374c85270887e4aa7a7c9eee8ce96e14c
reddy <1992pot@gmail.com>
2018-12-21 01:59:43 +0530

    this feature2.0 branch commit

---
+++ src/main/java/war/App1.java
@@ -0,0 +1,13 @@
+package war;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi this is Branch feature 2.0!" );
+    }
+}
